70b911c380e96c2cf6f8ec1ba2c7195612de51c7
davidrozwod <davidrozwod22@gmail.com>
2023-05-15 15:37:20 +1200

    Remove files specified in .gitignore

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace event_booking.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

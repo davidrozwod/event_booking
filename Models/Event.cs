e4e57075f9ba3a82e344ebba888b2f2e2bb38104
davidrozwod <davidrozwod22@gmail.com>
2023-05-16 08:53:41 +1200

    Database tables

---
+++ Models/Event.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace event_booking.Models;
+
+[Table("Events", Schema = "evnt")]
+public partial class Event
+{
+    [Key]
+    [Column("EventID")]
+    public int EventId { get; set; }
+
+    [Column("OrganizerID")]
+    public int OrganizerId { get; set; }
+
+    [Column("EventCategoryID")]
+    public int EventCategoryId { get; set; }
+
+    [StringLength(50)]
+    public string Name { get; set; } = null!;
+
+    [StringLength(250)]
+    public string? Description { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime StartDateTime { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime EndDateTime { get; set; }
+
+    [StringLength(100)]
+    public string? Image { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime? EarlyBirdCutoff { get; set; }
+
+    [ForeignKey("EventCategoryId")]
+    [InverseProperty("Events")]
+    public virtual EventCategory EventCategory { get; set; } = null!;
+
+    [ForeignKey("OrganizerId")]
+    [InverseProperty("Events")]
+    public virtual Organizer Organizer { get; set; } = null!;
+
+    [InverseProperty("Event")]
+    public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+
+    [InverseProperty("Event")]
+    public virtual ICollection<Vip> Vips { get; set; } = new List<Vip>();
+
+    [ForeignKey("EventId")]
+    [InverseProperty("Events")]
+    public virtual ICollection<AspNetUser> Ids { get; set; } = new List<AspNetUser>();
+}
---
+++ Models/EventCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace event_booking.Models;
+
+[Table("EventCategories", Schema = "evnt")]
+public partial class EventCategory
+{
+    [Key]
+    [Column("EventCategoryID")]
+    public int EventCategoryId { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string CategoryName { get; set; } = null!;
+
+    [InverseProperty("EventCategory")]
+    public virtual ICollection<Event> Events { get; set; } = new List<Event>();
+}

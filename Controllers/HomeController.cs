ee22cd9b46f2471cdf9bb02aab947fffc079ff58
davidrozwod <davidrozwod22@gmail.com>
2023-05-15 15:43:14 +1200

    Removed conflitcs

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using event_booking.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace event_booking.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult UserExclusiveFeatures()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

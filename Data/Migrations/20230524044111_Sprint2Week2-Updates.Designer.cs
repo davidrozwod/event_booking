e383adaca6c80b3ef2996211c18ef89dba0d6362
J Brokken <88115213+JeremyBrokken@users.noreply.github.com>
2023-05-24 17:29:18 +1200

    Migration prep

--- Data/Migrations/20230524044111_Sprint2Week2-Updates.Designer.cs
+++ Data/Migrations/20230524044111_Sprint2Week2-Updates.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using event_booking.Data;
 
@@ -11,9 +12,11 @@ using event_booking.Data;
 namespace event_booking.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230524044111_Sprint2Week2-Updates")]
+    partial class Sprint2Week2Updates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20230524044111_Sprint2Week2-Updates.cs
@@ -0,0 +1,458 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace event_booking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sprint2Week2Updates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HostsOrganizers",
+                schema: "evnt",
+                table: "Organizers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HostOrganizerCategories",
+                schema: "evnt",
+                table: "OrganizerCategories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VIP_ID",
+                schema: "evnt",
+                table: "VIP",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VenueID",
+                schema: "evnt",
+                table: "Venue",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketTypeID",
+                schema: "evnt",
+                table: "TicketType",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketID",
+                schema: "evnt",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketGroupID",
+                schema: "evnt",
+                table: "TicketGroup",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SectionID",
+                schema: "evnt",
+                table: "Section",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeatID",
+                schema: "evnt",
+                table: "Seats",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SaleID",
+                schema: "evnt",
+                table: "Sales",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseID",
+                schema: "evnt",
+                table: "Purchase",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizerID",
+                schema: "evnt",
+                table: "Organizers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizerCategoryID",
+                schema: "evnt",
+                table: "OrganizerCategories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupDiscountID",
+                schema: "evnt",
+                table: "GroupDiscounts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Picture",
+                schema: "evnt",
+                table: "EventUser",
+                type: "varchar(450)",
+                unicode: false,
+                maxLength: 450,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "evnt",
+                table: "EventUser",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "evnt",
+                table: "EventUser",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                schema: "evnt",
+                table: "EventUser",
+                type: "varchar(450)",
+                unicode: false,
+                maxLength: 450,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventID",
+                schema: "evnt",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventCategoryID",
+                schema: "evnt",
+                table: "EventCategories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DiscountID",
+                schema: "evnt",
+                table: "Discount",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Organizers",
+                schema: "evnt",
+                table: "Organizers",
+                column: "OrganizerID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrganizerCategories",
+                schema: "evnt",
+                table: "OrganizerCategories",
+                column: "OrganizerCategoryID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Organizers",
+                schema: "evnt",
+                table: "Organizers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrganizerCategories",
+                schema: "evnt",
+                table: "OrganizerCategories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VIP_ID",
+                schema: "evnt",
+                table: "VIP",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VenueID",
+                schema: "evnt",
+                table: "Venue",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketTypeID",
+                schema: "evnt",
+                table: "TicketType",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketID",
+                schema: "evnt",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketGroupID",
+                schema: "evnt",
+                table: "TicketGroup",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SectionID",
+                schema: "evnt",
+                table: "Section",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeatID",
+                schema: "evnt",
+                table: "Seats",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SaleID",
+                schema: "evnt",
+                table: "Sales",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PurchaseID",
+                schema: "evnt",
+                table: "Purchase",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizerID",
+                schema: "evnt",
+                table: "Organizers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizerCategoryID",
+                schema: "evnt",
+                table: "OrganizerCategories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupDiscountID",
+                schema: "evnt",
+                table: "GroupDiscounts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Picture",
+                schema: "evnt",
+                table: "EventUser",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(450)",
+                oldUnicode: false,
+                oldMaxLength: 450,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LastName",
+                schema: "evnt",
+                table: "EventUser",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FirstName",
+                schema: "evnt",
+                table: "EventUser",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Document",
+                schema: "evnt",
+                table: "EventUser",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(450)",
+                oldUnicode: false,
+                oldMaxLength: 450,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventID",
+                schema: "evnt",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventCategoryID",
+                schema: "evnt",
+                table: "EventCategories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DiscountID",
+                schema: "evnt",
+                table: "Discount",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HostsOrganizers",
+                schema: "evnt",
+                table: "Organizers",
+                column: "OrganizerID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HostOrganizerCategories",
+                schema: "evnt",
+                table: "OrganizerCategories",
+                column: "OrganizerCategoryID");
+        }
+    }
+}

d913ead6c34c61df17a4dfe6155f02715232fff9
J Brokken <88115213+JeremyBrokken@users.noreply.github.com>
2023-06-01 11:26:18 +1200

    State1

---
+++ Controllers/CRUDs/JunctionTicketVipsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using event_booking.Data;
+using event_booking.Models;
+
+public class JunctionTicketVipsController : Controller
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public JunctionTicketVipsController(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var junctionTicketVips = await _applicationDbContext.Junction_Ticket_VIP.ToListAsync();
+        return View(junctionTicketVips);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(JunctionTicketVip junctionTicketVip)
+    {
+        if (ModelState.IsValid)
+        {
+            await _applicationDbContext.JunctionTicketVips.AddAsync(junctionTicketVip);
+            await _applicationDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        return View(junctionTicketVip);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var junctionTicketVip = await _applicationDbContext.JunctionTicketVips.FindAsync(id);
+        if (junctionTicketVip == null)
+        {
+            return NotFound();
+        }
+
+        return View(junctionTicketVip);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(JunctionTicketVip junctionTicketVip)
+    {
+        if (ModelState.IsValid)
+        {
+            _applicationDbContext.Update(junctionTicketVip);
+            await _applicationDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        return View(junctionTicketVip);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var junctionTicketVip = await _applicationDbContext.JunctionTicketVips.FindAsync(id);
+        if (junctionTicketVip == null)
+        {
+            return NotFound();
+        }
+
+        _applicationDbContext.JunctionTicketVips.Remove(junctionTicketVip);
+        await _applicationDbContext.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+}

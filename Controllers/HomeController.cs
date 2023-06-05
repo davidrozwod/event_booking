f85a2e64d938a54e07e26cc776761a861c551547
Vincent-Glen <vincentg130@gmail.com>
2023-06-05 21:53:53 +1200

    Sub Categories Pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,7 +35,114 @@ namespace event_booking.Controllers
         {
             return View();
         }
+        public IActionResult AlternativeRock()
+        {
+            return View();
+        }
+        public IActionResult RapAndHipHop()
+        {
+            return View();
+        }
+        public IActionResult RBandSoul()
+        {
+            return View();
+        }
+        public IActionResult JazzAndBlues()
+        { return View();
+        }
+        public IActionResult HardRockMetal()
+        {
+            return View();
+        }
+        public IActionResult DanceElectronic()
+        {
+            return View();
+        }
         public IActionResult FamilyAndEducation()
+        {
+            return View();
+        }
+        public IActionResult CombatSports()
+        {
+            return View();
+        }
+        public IActionResult Baseball()
+        {
+            return View();
+        }
+        public IActionResult Basketball()
+        {
+            return View();
+        }
+        public IActionResult RugbyUnion()
+        {
+            return View();
+        }
+        public IActionResult Football()
+        {
+            return View();
+        }
+        public IActionResult Cricket()
+        {
+            return View();
+        }
+        public IActionResult Netball()
+        {
+            return View();
+        }
+        public IActionResult Golf()
+        {
+            return View();
+        }
+        public IActionResult BalletAndDance()
+        {
+            return View();
+        }
+        public IActionResult Classical()
+        {
+            return View();
+        }
+        public IActionResult Fashion()
+        {
+            return View();
+        }
+        public IActionResult MuseumAndExhibits()
+        {
+            return View();
+        }
+        public IActionResult Musicals()
+        {
+            return View();
+        }
+        public IActionResult Opera()
+        {
+            return View();
+        }
+        public IActionResult Plays()
+        {
+            return View();
+        }
+        public IActionResult ChildrensMusicAndTheater()
+        {
+            return View();
+        }
+        public IActionResult Circus()
+        {
+            return View();
+        }
+        public IActionResult FairsAndFestivals()
+        {
+            return View();
+        }
+        public IActionResult FamilyAttractions ()
+        {
+            return View();
+        }
+        public IActionResult IceShows()
+        {
+            return View();
+        }
+        public IActionResult MagicShows()
         {
             return View();
         }

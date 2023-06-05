1df5e043774883b7e7ecd5318b06a4ee8a3d7d36
J Brokken <88115213+JeremyBrokken@users.noreply.github.com>
2023-06-06 11:44:14 +1200

    Added Column to EventUser DocumentFileName

---
+++ Data/Migrations/20230605234330_EventUserModelAddColumn2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace event_booking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventUserModelAddColumn2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentFileName",
+                schema: "evnt",
+                table: "EventUser",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocumentFileName",
+                schema: "evnt",
+                table: "EventUser");
+        }
+    }
+}

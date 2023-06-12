b4e38efb178df6d300934916a9beb21a275bdb84
J Brokken <88115213+JeremyBrokken@users.noreply.github.com>
2023-06-12 14:19:11 +1200

    Added Venues FK to Event Migration

---
+++ Data/Migrations/20230611225317_EventAddVenueFKColumn2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace event_booking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventAddVenueFKColumn2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_VenueID",
+                schema: "evnt",
+                table: "Events",
+                column: "VenueID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Venue_VenueID",
+                schema: "evnt",
+                table: "Events",
+                column: "VenueID",
+                principalSchema: "evnt",
+                principalTable: "Venue",
+                principalColumn: "VenueID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Venue_VenueID",
+                schema: "evnt",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_VenueID",
+                schema: "evnt",
+                table: "Events");
+        }
+    }
+}

c1a4c32b64b21d3205a6a875a42623311d022186
J Brokken <88115213+JeremyBrokken@users.noreply.github.com>
2023-06-19 22:25:23 +1200

    runining

---
+++ Models/ViewModels/TicketSaleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace event_booking.Models.ViewModels
+{
+    public class TicketSaleViewModel
+    {
+        public Ticket Ticket { get; set; }
+        public EventUser EventUser { get; set; }
+    }
+}
